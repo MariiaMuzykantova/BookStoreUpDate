954d65e49eacdd2b08cdafc71a908d1109061966
MariiaMuzykantova <muzykantovamariia@hotmail.com>
2018-03-02 13:32:41 +0200

    commit 2

--- src/main/java/fi/haagahelia/BookStore/web/BookController.java
+++ src/main/java/fi/haagahelia/BookStore/web/BookController.java
@@ -1,10 +1,15 @@
 package fi.haagahelia.BookStore.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import fi.haagahelia.BookStore.domain.Book;
 import fi.haagahelia.BookStore.domain.BookRepository;
@@ -26,11 +31,23 @@ public String books(Model model) {
 	model.addAttribute("books",repository.findAll()); 
 	return "book"; //name of html 
 	} 
+
+@RequestMapping(value="/booksRest", method = RequestMethod.GET)
+public @ResponseBody List<Book> bookListRest() {
+return (List<Book>) repository.findAll();
+}
+
 @RequestMapping("/delete/{id}") 
 public String deleteBook(@PathVariable("id") Long bookid, Model model) { 
 repository.delete(bookid); 
 return "redirect:../books"; // not html 
 } 
+
+@RequestMapping(value="/booksRest/{id}", method = RequestMethod.GET)
+public @ResponseBody Book findStudentRest(@PathVariable("id") Long bookid) {
+return repository.findOne(bookid);
+}
+
 
 @RequestMapping("/add") 
 public String addBook(Model model) { 
